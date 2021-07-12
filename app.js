54d8a52fb861b5aa03d3817db00ce01859ee9736
jasdeepdhillon13 <jsdhillon3@gmail.com>
2021-07-11 19:06:15 -0700

    added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); 
+var compression = require('compression');
+var helmet = require('helmet');
+
 
 var app = express();
 
 //Set up default mongoose connection 
-var mongoDB = "mongodb+srv://firefox:mongodb13@cluster0.3citq.mongodb.net/local_library?retryWrites=true&w=majority"
+var dev_db_url = "mongodb+srv://firefox:mongodb13@cluster0.3citq.mongodb.net/local_library?retryWrites=true&w=majority"
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 
 mongoose.connect(mongoDB, {userNewUrlParse: true}); 
 
@@ -31,11 +36,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog',catalogRouter); 
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
